fa1a9690a250010cee28be313bc78cf7f9e7a1bf
kishanverma24 <kishanverma9151@gmail.com>
2024-06-02 20:52:49 +0530

    Added the login feature

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
 
 function Footer() {
+  const hours = [
+    {
+      id: 1,
+      day: "Mon-Sat",
+      time: "24hrs",
+    },
+
+    {
+      id: 2,
+      day: "Sunday",
+      time: "10:00 AM - 10:00 PM",
+    },
+  ];
   return (
-    <div>Footer</div>
-  )
+<>
+      <footer className={"container"}>
+        <hr />
+        <div className="content">
+          <div>
+            <img src="/logo.png" alt="logo" className="logo-img" />
+          </div>
+          <div>
+            <h4>Quick Links</h4>
+            <ul>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/appointment"}>Appointment</Link>
+              <Link to={"/about"}>About</Link>
+            </ul>
+          </div>
+          <div>
+            <h4>Hours</h4>
+            <ul>
+              {hours.map((element) => (
+                <li key={element.id}>
+                  <span>{element.day}</span>
+                  <span>{element.time}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4>Contact</h4>
+            <div>
+              <FaPhone />
+              <span>108</span>
+            </div>
+            <div>
+              <MdEmail />
+              <span>aiims@gmail.com</span>
+            </div>
+            <div>
+              <FaLocationArrow />
+              <span>Lucknow,India</span>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>  )
 }
 
 export default Footer
